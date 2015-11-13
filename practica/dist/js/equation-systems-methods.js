d0270a579556ea83deb1f1a012188c649a49c924
Thormod <thjodhild@gmail.com>
2015-11-12 19:48:47 -0500

    Gauss simple

--- practica/dist/js/equation-systems-methods.js
+++ practica/dist/js/equation-systems-methods.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
     		for (var j = 0; j < rows; j++) {
     			if(i==rows){
-    				equ += '<input class="form-control" style="width=30px; background-color: #3530f2; color: #fff;" id="matrixi'+i+'j'+j+'" value="'+Math.random()+'"><br>';
+    				equ += '<input class="form-control" style="width=30px; background-color: #286090; color: #fff;" id="matrixi'+i+'j'+j+'" value="'+Math.random()*10+'"><br>';
     			}else{
-    				equ += '<input class="form-control" style="width=30px;" id="matrixi'+i+'j'+j+'" value="'+Math.random()+'"><br>';
+    				equ += '<input class="form-control" style="width=30px;" id="matrixi'+i+'j'+j+'" value="'+Math.random()*10+'"><br>';
     			}
     		};
     		equ += '</div>';
@@ -32,37 +32,54 @@ $(document).ready(function() {
     	var f = new Array();
     	for (var i = 0; i <= rows; i++) {
     		f[i] = new Array();
-    		for (var j = 0; j < rows; j++) {
+    		for (var j = 0; j <= rows; j++) {
     			f[i][j] = $("#matrix"+'i'+i+'j'+j).val();
     		};
     	};
     	return f;
 }
+function mat_out(f){
+    var rows = $("#rows").val();
+    var equ = "";
+
+    for (var i = 0; i <= rows; i++) {
+        equ ='<div class="col-lg-2" >';
+        for (var j = 0; j < rows; j++) {
+            if(i == rows){
+                equ += '<input class="form-control" style="width=30px; background-color: #286090; color: #fff;" value="'+f[i][j]+'"  id="'+i+'_'+j+'"><br>';
+            }else{
+                equ += '<input class="form-control" style="width=30px;" value="'+f[i][j]+'"  id="'+i+'_'+j+'"><br>';
+            }
+        };
+       equ += '</div>';
+       $("#mat").append(equ);
+    };
+}
+
+
 function regressive_replacement(){
 
 }
 
 function simple_gauss(){
-
+    $("#mat").empty();
 	var rows = $("#rows").val();
 	var f = new Array();
 	f = matrixFill();
+
 	var multiplier = 1;
-	for (var k = 0; k < rows-1 ; k++) {
-		for (var i = k+1; i < rows ; i++) {
-			multplier = parseFloat(f[i][k])/parseFloat(f[k][k]);
-			for(var j = k; i <= rows ; i++){
-				f[i][j] = f[i][j] - parseFloat(multiplier) * parseFloat(f[k][j]);
-			};
-		};
-	};
 
-	for (var i = 0; i <= rows; i++) {
-    	
-
-    		for (var j = 0; j < rows; j++) {
-    			console.log(f[i][j]);
-    		};
-    	};
-
+    for (var k = 0; k < parseFloat(rows)-parseFloat(1); k++) {
+        for (var i = k+1; i < rows; i++) {
+            multiplier = parseFloat(f[k][i])/parseFloat(f[k][k]);   
+            for (var j = k; j <= rows; j++) {
+                console.log('k '+k+' i '+i +' j '+ j);
+                f[j][i] = parseFloat(f[j][i]) - parseFloat(multiplier)*parseFloat(f[j][k]);
+                console.log('mult '+multiplier);
+                console.log(f[i][j]);
+            };
+        };
+    };
+    
+    mat_out(f);
 }
